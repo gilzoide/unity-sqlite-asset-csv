54f88df6ca8a0742f162cb7e6a813be936dc6158
gilzoide <gilzoide@gmail.com>
2024-01-19 17:53:52 -0300

    Add documentation to ImportCsvToTable

--- Runtime/SQLiteConnectionExtensions.cs
+++ Runtime/SQLiteConnectionExtensions.cs
@@ -7,8 +7,23 @@ namespace Gilzoide.SqliteAsset.Csv
 {
     public static class SQLiteConnectionExtensions
     {
+        /// <summary>
+        /// Import a CSV data stream into the table named <paramref name="tableName"/> inside the database.
+        /// The table will be created if it doesn't exist yet.
+        /// </summary>
+        /// <param name="db">Open database connection</param>
+        /// <param name="tableName">Name of the table that should be filled with data from the CSV data stream.</param>
+        /// <param name="csvStream">Data stream with CSV-formatted contents.</param>
+        /// <param name="separator">Separator used for parsing the CSV. Defaults to comma.</param>
+        /// <param name="maxFieldSize">Maximum field size allowed.</param>
+        /// <exception cref="ArgumentNullException">Thrown if any of <paramref name="db"/>, <paramref name="tableName"/> and <paramref name="csvStream"/> are null.</exception>
+        /// <exception cref="CsvException">Thrown if an error is found while parsing the CSV data.</exception>
         public static void ImportCsvToTable(this SQLiteConnection db, string tableName, StreamReader csvStream, CsvReader.SeparatorChar separator = CsvReader.SeparatorChar.Comma, int maxFieldSize = int.MaxValue)
         {
+            if (db == null)
+            {
+                throw new ArgumentNullException(nameof(db));
+            }
             if (string.IsNullOrWhiteSpace(tableName))
             {
                 throw new ArgumentNullException(nameof(tableName));
